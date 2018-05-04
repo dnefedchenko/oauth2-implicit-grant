03e5df669e28d225e04accb90c5de7782591adc1
Dmitriy Nefedchenko <dnefedchenko@gmail.com>
2018-05-04 17:31:27 +0300

    Successful authentication handler implemented

--- src/main/java/com/freeman/oauth2/configuration/security/filters/ImplicitGrantAuthenticationFilter.java
+++ src/main/java/com/freeman/oauth2/configuration/security/filters/ImplicitGrantAuthenticationFilter.java
@@ -7,10 +7,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.util.StringUtils;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -49,5 +52,12 @@ public class ImplicitGrantAuthenticationFilter extends AbstractAuthenticationPro
         if (StringUtils.isEmpty(request.getParameter(googleAccessTokenParameter))) {
             throw new RuntimeException(String.format("Request parameter missing - %s", googleAccessTokenParameter));
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authResult);
+        chain.doFilter(request, response);
     }
 }
---
+++ src/main/java/com/freeman/oauth2/controllers/AuthenticationController.java
@@ -0,0 +1,14 @@
+package com.freeman.oauth2.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    @PostMapping(value = "/token")
+    public String obtainAccessToken() {
+        return "jwt";
+    }
+}
